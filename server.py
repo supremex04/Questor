67bada2dcb2e34fab010cfb533101ef8a1ff6a89
supremex04 <bhandarisupreme7@gmail.com>
2024-06-17 09:05:06 +0545

    server works

--- server.py
+++ server.py
@@ -1,87 +1,85 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
-import time
-from dotenv import load_dotenv 
-from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import JsonOutputParser
-from langchain_core.output_parsers import StrOutputParser
 import os
-from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from langchain_groq import ChatGroq
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.graph import StateGraph
+from langchain.schema import Document
+from llama_index.llms.groq import Groq
+from llama_index.core import Settings, VectorStoreIndex, StorageContext
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+import qdrant_client
 from typing_extensions import TypedDict
 from typing import List
-from pprint import pprint
-from langgraph.graph import END, StateGraph
+import nest_asyncio
 
+# Load environment variables from .env file
 load_dotenv()
+
+# Initialize FastEmbedEmbeddings model
 embed_model = FastEmbedEmbeddings(model_name="BAAI/bge-base-en-v1.5")
+
+# Initialize Flask app and enable CORS
 server_app = Flask(__name__)
-CORS(server_app) 
+CORS(server_app)
 
-# Commented out IPython magic to ensure Python compatibility.
-# %pip3 install --upgrade --quiet  langchain-google-genai pillow
-os.environ["GOOGLE_API_KEY"]=os.getenv("GOOGLE_API_KEY")
-llm = ChatGoogleGenerativeAI(model="gemini-pro")
+# Apply nest_asyncio to enable async within Flask
+nest_asyncio.apply()
 
-
-
-
+# Set environment variables
+os.environ['LLAMA_PARSE_API_KEY'] = os.getenv("LLAMA_API_KEY")
+os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY")
 os.environ['LANGCHAIN_TRACING-V2'] = 'true'
 os.environ['LANGCHAIN_ENDPOINT'] = 'https://api.smith.langchain.com'
 os.environ['LANGCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
-os.environ['ANTHROPIC_API_KEY'] = os.getenv("ANTHROPIC_API_KEY")
+os.environ['TAVILY_API_KEY'] = os.getenv("TAVILY_API_KEY")
 
-urls = [
-    "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8060478/#:~:text=It%20has%20recorded%20its%20highest,at%20the%20end%20of%20lockdown."
-]
+# Initialize LlamaParse with API key and load documents
+from llama_parse import LlamaParse
+llama_parse_documents = LlamaParse(api_key=os.getenv("LLAMA_PARSE_API_KEY"), result_type="markdown").load_data("./context/legal/Constitution-of-Nepal.pdf")
 
-docs = [WebBaseLoader(url).load() for url in urls]
-docs_list = [item for sublist in docs for item in sublist]
-print(f"len of documents :{len(docs_list)}")
+# Initialize Groq model
+llm1 = Groq(model="mixtral-8x7b-32768", api_key=os.getenv("GROQ_API_KEY"))
 
-text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-    chunk_size=512, chunk_overlap=0
+# Set settings for llm and embed_model
+Settings.llm = llm1
+Settings.embed_model = embed_model
+
+# Initialize QdrantClient for vector store
+client = qdrant_client.QdrantClient(
+    "https://b28f151a-b950-461a-92ba-8094252908b9.us-east4-0.gcp.cloud.qdrant.io",
+    api_key=os.getenv("QDRANT_API_KEY")
 )
-doc_splits = text_splitter.split_documents(docs_list)
-print(f"length of document chunks generated :{len(doc_splits)}")
 
-vectorstore = Chroma.from_documents(documents=doc_splits,
-                                    embedding=embed_model,
-                                    collection_name="local-rag")
+# Initialize vector store and storage context
+vector_store = QdrantVectorStore(client=client, collection_name="legal_documents")
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
-retriever = vectorstore.as_retriever(search_kwargs={"k":2})
+# Build index from documents
+index = VectorStoreIndex.from_documents(documents=llama_parse_documents, storage_context=storage_context, show_progress=True)
 
+# Initialize retriever with k=3 for search
+retriever = index.as_retriever(search_kwargs={"k": 3})
 
-prompt = PromptTemplate(
-    template=""" You are an expert at routing a
-    user question to a vectorstore or web search. Use the vectorstore if the question and the documents
-    match each other . It doesnot need to be a stringent test . Otherwise, use web-search. Give a binary choice 'web_search'
-    or 'vectorstore' based on it.The goal is to find whether the document is relevant to the questio or not . Return the a JSON with a single key 'datasource' and
-    no premable or explaination. Question to route: {question} docmunet to look for{document} <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
-    input_variables=["question","document"],
+# Initialize ChatGroq for question answering
+llm = ChatGroq(
+    temperature=0,
+    model="mixtral-8x7b-32768",
+    api_key=os.getenv("GROQ_API_KEY")
 )
-start = time.time()
-question_router = prompt | llm | JsonOutputParser()
-# question = "covid 19 deaths in nepal"
-# docs = retriever.invoke(question)
-# print(question_router.invoke({"question": question,"document":docs}))
-# end = time.time()
-# print(f"The time required to generate response by Router Chain in seconds:{end - start}")
 
-# docs[1].page_content
-
-# generation
-prompt = PromptTemplate(
-    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are an assistant for question-answering tasks.
-    Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know.
-    Use three sentences maximum and keep the answer concise <|eot_id|><|start_header_id|>user<|end_header_id|>
+# Generation prompt template
+generation_prompt = PromptTemplate(
+    template="""system You are an assistant for question-answering tasks. Only
+    Use the following pieces of retrieved context to answer the question. If context does not provide information to answer that question, just say user to do web search.
+    If the context provides needed information to answer the question, use it to answer the question keeping the answer concise. user
     Question: {question}
     Context: {context}
-    Answer: <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+    Answer: assistant""",
     input_variables=["question", "document"],
 )
 
@@ -90,262 +88,90 @@ def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 # Chain
-start = time.time()
-rag_chain = prompt | llm | StrOutputParser()
-# question = "different types of human memory"
-# docs = retriever.invoke(question)
-# generation = rag_chain.invoke({"context":docs,"question": question})
-# print(generation)
-# end = time.time()
-# print(f"The time required to generate response by Router Chain in seconds:{end - start}")
+rag_chain = generation_prompt | llm | StrOutputParser()
 
-# docs
-
-# retrieval grader
-prompt = PromptTemplate(
-    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing relevance
-    of a retrieved document to a user question. If the document contains keywords related to the user question,
-    grade it as relevant. It does not need to be a stringent test. The goal is to filter out erroneous retrievals. \n
-    Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question. \n
-    Provide the binary score as a JSON with a single key 'score' and no premable or explaination.
-     <|eot_id|><|start_header_id|>user<|end_header_id|>
-    Here is the retrieved document: \n\n {document} \n\n
-    Here is the user question: {question} \n <|eot_id|><|start_header_id|>assistant<|end_header_id|>
-    """,
-    input_variables=["question", "document"],
-)
-start = time.time()
-retrieval_grader = prompt | llm | JsonOutputParser()
-# question = "how the human brain works?"
-# docs = retriever.invoke(question)
-# doc_txt = docs[1].page_content
-# print(retrieval_grader.invoke({"question": question, "document": doc_txt}))
-# end = time.time()
-# print(f"The time required to generate response by the retrieval grader in seconds:{end - start}")
-
-# hallucination grader
-prompt = PromptTemplate(
-    template=""" <|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing whether
-    an answer is grounded in / supported by a set of facts. Give a binary 'yes' or 'no' score to indicate
-    whether the answer is grounded in / supported by a set of facts. Provide the binary score as a JSON with a
-    single key 'score' and no preamble or explanation. <|eot_id|><|start_header_id|>user<|end_header_id|>
-    Here are the facts:
-    \n ------- \n
-    {documents}
-    \n ------- \n
-    Here is the answer: {generation}  <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
-    input_variables=["generation", "documents"],
-)
-start = time.time()
-hallucination_grader = prompt | llm | JsonOutputParser()
-# hallucination_grader_response = hallucination_grader.invoke({"documents": docs, "generation": generation})
-# end = time.time()
-# print(f"The time required to generate response by the generation chain in seconds:{end - start}")
-# print(hallucination_grader_response)
-
-# answer grader
-prompt = PromptTemplate(
-    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing whether an
-    answer is useful to resolve a question. Give a binary score 'yes' or 'no' to indicate whether the answer is
+# Answer grading prompt template
+grading_prompt = PromptTemplate(
+    template="""system You are a grader assessing whether an
+    answer is useful to resolve a question. Give a score 'yes' or 'no' to indicate whether the answer is
     useful to resolve a question. Provide the binary score as a JSON with a single key 'score' and no preamble or explanation.
-     <|eot_id|><|start_header_id|>user<|end_header_id|> Here is the answer:
+    user Here is the answer:
     \n ------- \n
     {generation}
     \n ------- \n
-    Here is the question: {question} <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+    Here is the question: {question} assistant""",
     input_variables=["generation", "question"],
 )
-start = time.time()
-answer_grader = prompt | llm | JsonOutputParser()
-# answer_grader_response = answer_grader.invoke({"question": question,"generation": generation})
-# end = time.time()
-# print(f"The time required to generate response by the answer grader in seconds:{end - start}")
-# print(answer_grader_response)
 
-# web search
+# Chain for answer grading
+answer_grader = grading_prompt | llm | JsonOutputParser()
 
-os.environ['TAVILY_API_KEY'] = os.getenv("TAVILY_API_KEY")
+# Initialize TavilySearchResults for web search tool with k=3
 web_search_tool = TavilySearchResults(k=3)
 
-### State
-
+# Define GraphState TypedDict
 class GraphState(TypedDict):
-    question : str
-    generation : str
-    web_search : str
-    documents : List[str]
+    question: str
+    generation: str
+    web_search: str
+    documents: List[str]
 
-from langchain.schema import Document
-def retrieve(state):
-    print("---RETRIEVE---")
-    question = state["question"]
-
-    # Retrieval
-    documents = retriever.invoke(question)
-    return {"documents": documents, "question": question}
-#
+# Define generate function for StateGraph
 def generate(state):
-
-    print("---GENERATE---")
     question = state["question"]
-    documents = state["documents"]
+    documents = state.get("documents", [])
 
-    # RAG generation
-    generation = rag_chain.invoke({"context": documents, "question": question})
-    return {"documents": documents, "question": question, "generation": generation}
-#
-def grade_documents(state):
+    # Query engine for retrieving documents
+    query_engine = index.as_query_engine()
+    generation = query_engine.query(question).response
+    print("GENERATING FROM CONTEXT")
 
-    print("---CHECK DOCUMENT RELEVANCE TO QUESTION---")
-    question = state["question"]
-    documents = state["documents"]
+    # Evaluate score using answer grader
+    score = answer_grader.invoke({"question": question, "generation": generation})
+    print(score['score'])
 
-    # Score each doc
-    filtered_docs = []
-    web_search = "No"
-    for d in documents:
-        score = retrieval_grader.invoke({"question": question, "document": d.page_content})
-        grade = score['score']
-        # Document relevant
-        if grade.lower() == "yes":
-            print("---GRADE: DOCUMENT RELEVANT---")
-            filtered_docs.append(d)
-        # Document not relevant
-        else:
-            print("---GRADE: DOCUMENT NOT RELEVANT---")
-            # We do not include the document in filtered_docs
-            # We set a flag to indicate that we want to run web search
-            web_search = "Yes"
-            continue
-    return {"documents": filtered_docs, "question": question, "web_search": web_search}
-#
-def web_search(state):
+    if score['score'] == 'yes':
+        print("CONTEXT RESPONSE IS OK")
+        return {"documents": documents, "question": question, "generation": generation}
 
-    print("---WEB SEARCH---")
-    question = state["question"]
-    documents = state["documents"]
-
-    # Web search
-    docs = web_search_tool.invoke({"query": question})
-    web_results = "\n".join([d["content"] for d in docs])
-    web_results = Document(page_content=web_results)
-    if documents is not None:
+    print("DOING WEB SEARCH")
+    while score['score'] == 'no':
+        docs = web_search_tool.invoke({"query": question})
+        web_results = "\n".join([d["content"] for d in docs])
+        web_results = Document(page_content=web_results)
         documents.append(web_results)
-    else:
-        documents = [web_results]
-    return {"documents": documents, "question": question}
 
-# conditional edges
-def route_question(state):
+        generation = rag_chain.invoke({"context": format_docs(documents), "question": question})
+        print("GENERATING FROM WEB_SEARCH")
+        
+        score = answer_grader.invoke({"question": question, "generation": generation})
+        if score['score'] == 'yes':
+            print("WEB SEARCH RESULT IS OK")
+            return {"documents": documents, "question": question, "generation": generation}
 
-    print("---ROUTE QUESTION---")
-    question = state["question"]
-    documents = retriever.invoke(question)
-    print(question)
-    source = question_router.invoke({"question": question,"document":documents})
-    print(source)
-    print(source['datasource'])
-    if source['datasource'] == 'web_search':
-        print("---ROUTE QUESTION TO WEB SEARCH---")
-        return "websearch"
-    elif source['datasource'] == 'vectorstore':
-        print("---ROUTE QUESTION TO RAG---")
-        return "vectorstore"
-
-def decide_to_generate(state):
-
-    print("---ASSESS GRADED DOCUMENTS---")
-    question = state["question"]
-    web_search = state["web_search"]
-    filtered_documents = state["documents"]
-
-    if web_search == "Yes":
-        # All documents have been filtered check_relevance
-        # We will re-generate a new query
-        print("---DECISION: ALL DOCUMENTS ARE NOT RELEVANT TO QUESTION, INCLUDE WEB SEARCH---")
-        return "websearch"
-    else:
-        # We have relevant documents, so generate answer
-        print("---DECISION: GENERATE---")
-        return "generate"
-
-def grade_generation_v_documents_and_question(state):
-
-    print("---CHECK HALLUCINATIONS---")
-    question = state["question"]
-    documents = state["documents"]
-    generation = state["generation"]
-
-    score = hallucination_grader.invoke({"documents": documents, "generation": generation})
-    grade = score['score']
-
-    # Check hallucination
-    if grade == "yes":
-        print("---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---")
-        # Check question-answering
-        print("---GRADE GENERATION vs QUESTION---")
-        score = answer_grader.invoke({"question": question,"generation": generation})
-        grade = score['score']
-        if grade == "yes":
-            print("---DECISION: GENERATION ADDRESSES QUESTION---")
-            return "useful"
-        else:
-            print("---DECISION: GENERATION DOES NOT ADDRESS QUESTION---")
-            return "not useful"
-    else:
-        print("---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS, RE-TRY---")
-        return "not supported"
-
-# adding notes
+# Initialize StateGraph workflow
 workflow = StateGraph(GraphState)
+workflow.add_node("generate", generate)
+workflow.set_entry_point("generate")
+workflow.set_finish_point("generate")
 
-# Define the nodes
-workflow.add_node("websearch", web_search) # web search
-workflow.add_node("retrieve", retrieve) # retrieve
-workflow.add_node("grade_documents", grade_documents) # grade documents
-workflow.add_node("generate", generate) # generatae
-
-workflow.set_conditional_entry_point(
-    route_question,
-    {
-        "websearch": "websearch",
-        "vectorstore": "retrieve",
-    },
-)
-
-workflow.add_edge("retrieve", "grade_documents")
-workflow.add_conditional_edges(
-    "grade_documents",
-    decide_to_generate,
-    {
-        "websearch": "websearch",
-        "generate": "generate",
-    },
-)
-workflow.add_edge("websearch", "generate")
-workflow.add_conditional_edges(
-    "generate",
-    grade_generation_v_documents_and_question,
-    {
-        "not supported": "generate",
-        "useful": END,
-        "not useful": "websearch",
-    },
-)
-
+# Compile app from StateGraph
 app = workflow.compile()
 
+# Route to handle POST requests to '/query'
 @server_app.route('/query', methods=['POST'])
 def query():
     data = request.json
     question = data['question']
+    print(f"Received question: {question}")
     inputs = {"question": question}
     output = None
     for output in app.stream(inputs):
         for key, value in output.items():
-            pprint(f"Finished running: {key}:")
-    pprint(value["generation"])
+            print(f"Finished running: {key}")
+    print(f"Generated answer: {value['generation']}")
     return jsonify({"generation": value["generation"]})
-    
+
+# Main entry point of the application
 if __name__ == "__main__":
-    server_app.run(debug=True)
+    server_app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
